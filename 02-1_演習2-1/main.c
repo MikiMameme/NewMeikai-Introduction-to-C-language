2e33fd2471aa6019ad9d645ff4bd30cbb18a4c3a
MikiMameme <milky.is.mama.flavor@gmail.com>
2025-06-10 21:52:24 +0900

    02-1_演習2-1

---
+++ 02-1_演習2-1/main.c
@@ -0,0 +1,14 @@
+/* 二つの整数値を読み込んで前者の数が後者の数の何パーセントであるかを表示する。 */
+#include <stdio.h>
+int main(void)
+{
+	int x, y;
+	
+	puts("二つの整数を入力してください。");
+	printf("整数x :");	scanf("%d", &x);
+	printf("整数y :");	scanf("%d", &y);
+	
+	printf("xの値はyの%.0f%%です\n", (double)x / (double)y * 100); 
+												/* どうやら、ここまでしなくても良かったみたい。 */
+	return 0;
+}
