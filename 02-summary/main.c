be195587f4bd549559ead0d8b24fe7022ffc6fba
MikiMameme <milky.is.mama.flavor@gmail.com>
2025-06-11 22:43:05 +0900

    02-summary

---
+++ 02-summary/main.c
@@ -0,0 +1,31 @@
+/* 第二章のまとめ */
+
+#include <stdio.h>
+
+int main(void)
+{
+	int a;
+	int b;
+	
+	double r;
+	
+	printf("整数aとbの値 :");
+	scanf("%d%d", &a, &b);
+	
+	printf("a + b = %d\n", a + b); /* 加算：2項 + 演算子 */
+	printf("a - b = %d\n", a - b); /* 加減算：2項 - 演算子 */
+	printf("a * b = %d\n", a * b); /* 積：2項 * 演算子 */
+	printf("a / b = %d\n", a / b); /* 商：/演算子 */
+	printf("a %% b = %d\n", a % b); /* 剰余：%演算子 */
+	
+	printf("(a+b)/2 = %d\n",	(a + b) / 2);
+	
+	printf("平均値 = %f\n\n", (double)(a+b) / 2);
+	
+	printf("半径 :");
+	scanf("%lf", &r);
+	
+	printf("半径%.3fの円の面積は%.3fです。\n", r, 3.14 * r * r);
+	
+	return 0;
+}
