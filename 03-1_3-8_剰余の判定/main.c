b0bd119b4a6d5bbff0acfd8c5cc3e5d8d3de6545
神酒まめ（Miki Mame） <milky.is.mama.flavor@gmail.com>
2025-06-14 17:29:04 +0900

    03-1_3-8_剰余の判定

---
+++ 03-1_3-8_剰余の判定/main.c
@@ -0,0 +1,18 @@
+/* 読み込んだ整数値の最下位桁は5であるか */
+
+#include<stdio.h>
+
+int main(void)
+{
+	int num;
+	
+	printf("整数を入力してください :");
+	scanf("%d", &num);
+	
+	if((num % 10) == 5)
+		puts("再開の桁は5です。");
+	else
+		puts("最下位の桁は5ではありません。");
+		
+	return 0;
+}
