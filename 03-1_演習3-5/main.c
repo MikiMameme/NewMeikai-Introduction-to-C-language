76c76606fb06fbe52b23137639f677ad7e138260
神酒まめ（Miki Mame） <milky.is.mama.flavor@gmail.com>
2025-06-15 20:26:19 +0900

    03-1_演習3-5

---
+++ 03-1_演習3-5/main.c
@@ -0,0 +1,24 @@
+/* 等価演算子と官憲演算子が生成する値を表示 */
+
+#include<stdio.h>
+
+int main(void)
+{
+	int a, b;
+	
+	puts("二つの整数を入力してください。");
+	printf("変数a :");	scanf("%d", &a);
+	printf("変数b :");	scanf("%d", &b);
+	
+	puts("等価式の値");
+	printf("a == b の値 : %d\n", a == b);
+	printf("a != b の値 : %d\n", a != b);
+	
+	puts("関係式の値");
+	printf("a <  b の値 : %d\n", a <  b);
+	printf("a <= b の値 : %d\n", a <= b);
+	printf("a >  b の値 : %d\n", a >  b);
+	printf("a >= b の値 : %d\n", a >= b);
+	
+	return 0;
+}
