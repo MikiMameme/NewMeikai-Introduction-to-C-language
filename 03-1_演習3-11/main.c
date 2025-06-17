edb5daf060de4e815cae138963db74bbdd1ef3e8
神酒まめ（Miki Mame） <milky.is.mama.flavor@gmail.com>
2025-06-17 18:22:21 +0900

    03-1_演習3-11（読み込んだ二つの整数値の差が10以下かどうかを表示）
    読み込んだ二つの整数値の差が10以下かどうかを表示

---
+++ 03-1_演習3-11/main.c
@@ -0,0 +1,21 @@
+/* 読み込んだ二つの整数値の差が10以下かどうかを表示 */
+
+#include<stdio.h>
+
+int main(void)
+{
+	int a, b, diff;
+	
+	puts("二つの整数を入力してください。");
+	printf("整数Ａ :");	scanf("%d", &a);
+	printf("整数Ｂ :");	scanf("%d", &b);
+	
+	diff = a - b;
+	
+	if(diff > 10 || diff < -10)
+		puts("それらの差は11以上です。");
+	else
+		puts("それらの差は10以下です。");
+	
+	return 0;
+}
