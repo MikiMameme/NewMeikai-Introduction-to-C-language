15a525f209310a4d3eca498cc421949da3f4cf17
神酒まめ（Miki Mame） <milky.is.mama.flavor@gmail.com>
2025-06-18 20:44:07 +0900

    04-2_4-8_一定間隔の繰り返し

---
+++ 04-2_4-8_一定間隔の繰り返し/main.c
@@ -0,0 +1,18 @@
+/* “Ç‚İ‚ñ‚¾®”‚ÌŒÂ”‚¾‚¯ * ‚ğ˜A‘±•\¦‚·‚é */
+
+#include<stdio.h>
+
+int main(void)
+{
+	
+	int no;
+	
+	printf("³‚Ì®”‚ğ“ü—Í‚µ‚Ä‚­‚¾‚³‚¢ :");
+	scanf("%d", &no);
+	
+	while(no-- > 0)
+		putchar('*');
+	putchar('\n');
+	
+	return 0;
+}
