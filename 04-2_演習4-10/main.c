186a1d2fc0caf32329ad68cc137210e844afd704
神酒まめ（Miki Mame） <milky.is.mama.flavor@gmail.com>
2025-06-19 18:54:29 +0900

    04-2_演習4-10

---
+++ 04-2_演習4-10/main.c
@@ -0,0 +1,17 @@
+/* “Ç‚İ‚ñ‚¾®”‚ÌŒÂ”‚¾‚¯ * ‚ğ˜A‘±•\¦‚·‚é */
+
+#include<stdio.h>
+
+int main(void)
+{
+	
+	int no;
+	
+	printf("³‚Ì®”‚ğ“ü—Í‚µ‚Ä‚­‚¾‚³‚¢ :");
+	scanf("%d", &no);
+	
+		while(no-- > 0)
+			puts("*");
+	
+	return 0;
+}
