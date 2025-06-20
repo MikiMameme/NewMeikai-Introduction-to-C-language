8c9f1e72b67c96ce9b9cc4e77f1e778bf6d71ca1
神酒まめ（Miki Mame） <milky.is.mama.flavor@gmail.com>
2025-06-20 20:34:33 +0900

    04-3_4-14_偶数の列挙

---
+++ 04-3_4-14_偶数の列挙/main.c
@@ -0,0 +1,17 @@
+/* “Ç‚İ‚ñ‚¾®”’lˆÈ‰º‚Ì‹ô”‚ğ•\¦ */
+
+#include<stdio.h>
+
+int main(void)
+{
+	int i, n;
+	
+	printf("®”’l :");
+	scanf("%d", &n);
+	
+	for(i = 2; i <= n; i += 2)
+		printf("%d", i);
+	putchar('\n');
+	
+	return 0;
+}
