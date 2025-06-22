88c8f48a05f1fe5152c23009304cff6b4b76af7d
神酒まめ（Miki Mame） <milky.is.mama.flavor@gmail.com>
2025-06-22 19:34:11 +0900

    04-4_演習4-24

---
+++ 04-4_演習4-24/main.c
@@ -0,0 +1,22 @@
+/* ピラミッドを描画 */
+
+#include<stdio.h>
+
+int main(void)
+{
+	int i, j;
+	int h;
+	
+	puts("ピラミッドを作ります。");
+	printf("何段ですか？ :");
+	scanf("%d", &h);
+	
+	for(i = 1; i <= h; i++){
+		for(j = 1; j <= h - i; j++)
+			putchar(' ');
+		for(j = 1; j <= 2 * i - 1; j++)
+			putchar('*');
+		putchar('\n');
+	}
+	return 0;
+}
