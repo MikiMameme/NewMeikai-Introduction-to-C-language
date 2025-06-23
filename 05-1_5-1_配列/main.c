84b4a2892a402b32b3891fcd65bd469f9a16f812
神酒まめ（Miki Mame） <milky.is.mama.flavor@gmail.com>
2025-06-23 21:18:12 +0900

    05-1_5-1_配列

---
+++ 05-1_5-1_配列/main.c
@@ -0,0 +1,25 @@
+/* 5人の学生の点数を読み込んで合計点と平均点を表示 */
+
+#include<stdio.h>
+
+int main(void)
+{
+	int uchida;
+	int satoh;
+	int sanaka;
+	int hiraki;
+	int masaki;
+	int sum = 0;
+	
+	printf("5人の点数を入力してください。 :\n");
+	printf(" 1番 :");	scanf("%d", &uchida);	sum += uchida;
+	printf(" 2番 :");	scanf("%d", &satoh);	sum += satoh;
+	printf(" 3番 :");	scanf("%d", &sanaka);	sum += sanaka;
+	printf(" 4番 :");	scanf("%d", &hiraki);	sum += hiraki;
+	printf(" 5番 :");	scanf("%d", &masaki);	sum += masaki;
+	
+	printf("合計点 :%5d\n", sum);
+	printf("合計点 :%5.1f\n", (double)sum / 5);
+	
+	return 0;
+}
