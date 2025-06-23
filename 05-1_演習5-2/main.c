9c04288da06ff35096fdc3470cee80cb0fbcecc3
神酒まめ（Miki Mame） <milky.is.mama.flavor@gmail.com>
2025-06-23 22:11:37 +0900

    05-1_演習5-2

---
+++ 05-1_演習5-2/main.c
@@ -0,0 +1,17 @@
+/* 配列の各要素に先頭から順に５、４、３、２、１を代入して表示（for文） */
+
+#include<stdio.h>
+
+int main(void)
+{
+	int i;
+	int v[5];				/* int[5]型の配列 */
+	
+	for(i = 0; i < 5; i++)	/* 要素に値を代入 */
+		v[i] = 5 - i;
+		
+	for(i = 0; i < 5; i++)	/* 要素の値を表示 */
+		printf("v[%d] = %d\n", i, v[i]);
+		
+	return 0;
+}
